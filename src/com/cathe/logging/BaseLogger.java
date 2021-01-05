3d6b9aedea195933b1ee480b7ed0c3d017b0973e
Ivan Reshetnyak <ivan.reshetnyak@gmail.com>
2021-01-05 18:41:53 +0300

    v0.3.2 - Move CopyFormatter to separate file

--- src/com/cathe/logging/BaseLogger.java
+++ src/com/cathe/logging/BaseLogger.java
@@ -1,22 +1,7 @@
 package com.cathe.logging;
 
-import java.util.Date;
-import java.util.logging.Formatter;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-
-class CopyFormatter extends Formatter {
-  private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
-
-  @Override
-  public String format( LogRecord record ) {
-    return String.format(format,
-            new Date(record.getMillis()),
-                     record.getLevel().getLocalizedName(),
-                     record.getMessage());
-  }
-}
 
 public class BaseLogger {
   protected Logger logger;
